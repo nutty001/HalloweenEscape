276f69d0332d27b4625b52f5463a3afa63eafd96
manslaughter <mnslaughter5@gmail.com>
2021-12-19 23:45:20 +0530

    enemy added

---
+++ Assets/Enemy/Scripts/batfly.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class batfly : MonoBehaviour
+{
+
+    public float speed;
+    private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+
+        if (Vector2.Distance(transform.position, target.position) > 2)
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+
+
+    }
+}
