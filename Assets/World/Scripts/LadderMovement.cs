66921979d30453505308ee8b0f132dbd75d2d7a2
nutty001 <pratikthapa328@gmail.com>
2021-12-19 22:09:16 +0530

    ladders working

---
+++ Assets/World/Scripts/LadderMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderMovement : MonoBehaviour
+{
+    public float climbingspeed = 6;
+
+    //LadderMovement
+    private void OnTriggerStay2D(Collider2D other)
+    { 
+        if(other.tag=="Player"&&Input.GetKey(KeyCode.W))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, climbingspeed);
+        }
+        else if (other.tag == "Player" && Input.GetKey(KeyCode.S))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -climbingspeed);
+        }
+        else
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0,1);
+        }
+    }
+}
